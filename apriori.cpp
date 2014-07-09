aa26a3d2b26bcc6c947f59340c51fc7bf8705958
przemyslawpipala <przemyslaw.pipala@gmail.com>
2014-07-09 15:13:54 +0200

    Apriori algorithm

---
+++ apriori.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int tab[4][4]={{1,3,4,0},{2,3,5,0},{1,2,3,5},{3,5,0,0}};
+int s1,s2,s3,s4,s5;
+int s1procent;
+int s2procent;
+int s3procent;
+int s4procent;
+int s5procent;
+int minsup=50;
+int minconf=80;
+string i1="-";
+string i2="-";
+string i3="-";
+string i4="-";
+string i5="-";
+
+main()
+{
+	cout << "C1={1,2,3,4,5}";
+	s1=0;
+	if (tab[0][0]==1) s1++;
+	if (tab[1][0]==1) s1++;
+	if (tab[2][0]==1) s1++;
+	if (tab[3][0]==1) s1++;
+	s1procent=((s1*100)/4);
+	cout << endl << "sup{1}="<<s1procent<<"%";
+	s2=0;
+	if (tab[0][0]==2) s2++;
+	if (tab[1][0]==2) s2++;
+	if (tab[2][0]==2) s2++;
+	if (tab[3][0]==2) s2++;
+	if (tab[0][1]==2) s2++;
+	if (tab[1][1]==2) s2++;
+	if (tab[2][1]==2) s2++;
+	if (tab[3][1]==2) s2++;
+	if (tab[0][2]==2) s2++;
+	if (tab[1][2]==2) s2++;
+	if (tab[2][2]==2) s2++;
+	if (tab[3][2]==2) s2++;
+	if (tab[0][3]==2) s2++;
+	if (tab[1][3]==2) s2++;
+	if (tab[2][3]==2) s2++;
+	if (tab[3][3]==2) s2++;
+	s2procent=((s2*100)/4);
+	cout << endl << "sup{2}="<<s2procent<<"%";
+	s3=0;
+	if (tab[0][0]==3) s3++;
+	if (tab[1][0]==3) s3++;
+	if (tab[2][0]==3) s3++;
+	if (tab[3][0]==3) s3++;
+	if (tab[0][1]==3) s3++;
+	if (tab[1][1]==3) s3++;
+	if (tab[2][1]==3) s3++;
+	if (tab[3][1]==3) s3++;
+	if (tab[0][2]==3) s3++;
+	if (tab[1][2]==3) s3++;
+	if (tab[2][2]==3) s3++;
+	if (tab[3][2]==3) s3++;
+	if (tab[0][3]==3) s3++;
+	if (tab[1][3]==3) s3++;
+	if (tab[2][3]==3) s3++;
+	if (tab[3][3]==3) s3++;
+	s3procent=((s3*100)/4);
+	cout << endl << "sup{3}="<<s3procent<<"%";
+	s4=0;
+	if (tab[0][0]==4) s4++;
+	if (tab[1][0]==4) s4++;
+	if (tab[2][0]==4) s4++;
+	if (tab[3][0]==4) s4++;
+	if (tab[0][1]==4) s4++;
+	if (tab[1][1]==4) s4++;
+	if (tab[2][1]==4) s4++;
+	if (tab[3][1]==4) s4++;
+	if (tab[0][2]==4) s4++;
+	if (tab[1][2]==4) s4++;
+	if (tab[2][2]==4) s4++;
+	if (tab[3][2]==4) s4++;
+	if (tab[0][3]==4) s4++;
+	if (tab[1][3]==4) s4++;
+	if (tab[2][3]==4) s4++;
+	if (tab[3][3]==4) s4++;
+	s4procent=((s4*100)/4);
+	cout << endl << "sup{4}="<<s4procent<<"%";
+	s5=0;
+	if (tab[0][0]==5) s5++;
+	if (tab[1][0]==5) s5++;
+	if (tab[2][0]==5) s5++;
+	if (tab[3][0]==5) s5++;
+	if (tab[0][1]==5) s5++;
+	if (tab[1][1]==5) s5++;
+	if (tab[2][1]==5) s5++;
+	if (tab[3][1]==5) s5++;
+	if (tab[0][2]==5) s5++;
+	if (tab[1][2]==5) s5++;
+	if (tab[2][2]==5) s5++;
+	if (tab[3][2]==5) s5++;
+	if (tab[0][3]==5) s5++;
+	if (tab[1][3]==5) s5++;
+	if (tab[2][3]==5) s5++;
+	if (tab[3][3]==5) s5++;
+	s5procent=((s5*100)/4);
+	cout << endl << "sup{5}="<<s5procent<<"%";
+	if (s1procent >= minsup) i1 = "1";
+	if (s2procent >= minsup) i2 = "2";
+	if (s3procent >= minsup) i3 = "3";
+	if (s4procent >= minsup) i4 = "4";
+	if (s5procent >= minsup) i5 = "5";
+	cout << endl << "L1={"<< i1 <<","<<i2 <<","<<i3 <<","<<i4 <<","<<i5<<"}";
+	getch();
+	return 0;
+}
